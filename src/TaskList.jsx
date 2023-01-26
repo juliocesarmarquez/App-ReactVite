a597cb88b3ef771bf0671760b092bd105608ec54
juliocesarmarquez <juliocesarmarquez88@gmail.com>
2023-01-26 12:09:18 -0300

    fix errors

---
+++ src/TaskList.jsx
@@ -0,0 +1,18 @@
+import TaskCard from './TaskCard'
+function TaskList({tasks}) {
+  
+
+  if (tasks.length === 0) {
+    return <h1>No existen tareas</h1>;
+  }
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskCard task={task}/>
+
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Mi primer tarea",
+        description:"la batata ",      
+    },
+    {
+        id: 1,
+        title: "Mi segunda tarea",
+        description:"la batata ",      
+    },
+    {
+        id: 2,
+        title: "Mi tercer tarea",
+        description:"la batata ",      
+    },
+];
